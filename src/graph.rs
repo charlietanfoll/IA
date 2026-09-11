25c27091cc3041b9adb84a94dd0941c2ae81f13f
Charlie TP. Lobo <u16827968@exatas.ffclrp.usp.br>
2026-09-11 17:27:04 -0300

    Modelagem inicial de dados para o grafo.

--- src/graph.rs
+++ src/graph.rs
@@ -1,2 +1,16 @@
 // Integrantes do Grupo:
 // - Charlie TP. Lobo (Nº USP: 16827968)
+
+use std::collections::HashMap;
+
+struct caminho {
+    origem: u32,
+    destino: u32,
+    custo: f32, // sujeira, nn gosto de numeros flutuantes...
+    nome: String,
+};
+
+pub struct graph {
+    nodes: HashMap<u32, Vec<caminho>>,
+};
+impl graph {};
